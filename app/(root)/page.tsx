62ecbdfa9ae111f6515b7e14d0639c3519ab6619
Elvin <elvinsrkrov@gmail.com>
2024-12-19 20:40:45 +0400

    File actions - rename, detail, delete, share, download features have implemented

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,9 @@
 const Dashboard = async () => {
-  return <div className="dashboard-container"></div>;
+  return (
+    <div className="dashboard-container">
+      <h2>StoreIt | The only storage solution you need</h2>
+    </div>
+  );
 };
 
 export default Dashboard;
